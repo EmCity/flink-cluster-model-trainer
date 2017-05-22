1a491629a3faad8a06289b95d4c3e4d83a7c2a84
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-05-22 18:51:25 +0200

    refactored/commented Last3MonthsSubmission and uploaded a submission_travelTime.csv

--- python/traffic-prediction/src/Last3MonthsSubmission.py
+++ python/traffic-prediction/src/Last3MonthsSubmission.py
@@ -3,29 +3,34 @@ import pandas as pd
 from datetime import datetime
 import TravelTimeSubmission as submission
 
-
-def from_index_to_prediction_day(day, hour, minute, endhour, endminute):
-    start = datetime(2016, 10, 18 + day, hour, minute, 0)
-    end = datetime(2016, 10, 18 + day, endhour, endminute, 0)
+def from_index_to_prediction_day(day, h):
+    start = datetime(2016, 10, 18 + day, h[0][0], h[0][1], 0)
+    end = datetime(2016, 10, 18 + day, h[1][0], h[1][1], 0)
     return "[" + str(start) + "," + str(end) + ")"
 
-#we are interested in all weekdays
-weekdays = [0,1,2,3,4,5,6]
+# we are interested in all weekdays
+weekdays = [0, 1, 2, 3, 4, 5, 6]
 
-#all 20 min windows
-times = [[(8,00),(8,20)],[(8,20),(8,40)],[(8,40),(9,00)],[(9,00),(9,20)],[(9,20),(9,40)],[(9,40),(10,00)],
-             [(15,00),(15,20)],[(15,20),(15,40)],[(15,40),(16,00)],[(16,00),(16,20)],[(16,20),(16,40)],[(16,40),(17,00)]]
+# all 20 min windows
+times = [[(8, 00), (8, 20)], [(8, 20), (8, 40)], [(8, 40), (9, 00)], [(9, 00), (9, 20)], [(9, 20), (9, 40)],[(9, 40), (10, 00)],
+         [(15, 00), (15, 20)], [(15, 20), (15, 40)], [(15, 40), (16, 00)], [(16, 00), (16, 20)], [(16, 20), (16, 40)],[(16, 40), (17, 00)]]
 
-dfResult = pd.DataFrame(columns=["intersection_id", "tollgate_id","avg_travel_time", "time_window"])
+# the result panda data frame
+dfResult = pd.DataFrame(columns=["intersection_id", "tollgate_id", "avg_travel_time", "time_window"])
+
+# iterate over all weekday - timeWindow pairs
 for day in weekdays:
-    for h in times:
-        df = traffic.get_traffic([day], (h[0], h[1]))
-        df3 = df.groupby(['intersection_id', 'tollgate_id'])['travel_time'].mean().reset_index(name="avg_travel_time")
-        df5 = pd.DataFrame(df3, columns=["intersection_id", "tollgate_id","avg_travel_time", "time_window"])
-        df5["time_window"] = from_index_to_prediction_day(day, h[0][0], h[0][1], h[1][0], h[1][1])
-        dfResult = pd.concat([dfResult, df5])
-dfResult = dfResult[["intersection_id", "tollgate_id", "time_window", "avg_travel_time"]]
-print(dfResult)
-submission.TravelTimeSubmission.save_df_travel_time_submission(submission.TravelTimeSubmission, df5)
+    for hours in times:
+        # calculate for each timewindow the avg_travel_time based on the data in this time window
+        df2 = traffic.get_traffic([day], (hours[0], hours[1])).groupby(['intersection_id', 'tollgate_id'])[
+            'travel_time'].mean().reset_index(name="avg_travel_time")
+        df3 = pd.DataFrame(df2, columns=["intersection_id", "tollgate_id", "avg_travel_time", "time_window"])
 
+        # add the time window column to the dataframe
+        df3["time_window"] = from_index_to_prediction_day(day, hours)
 
+        # add the time window data to the result data frame
+        dfResult = pd.concat([dfResult, df3], axis=0, ignore_index=1)
+
+dfResult = dfResult[["intersection_id", "tollgate_id", "avg_travel_time", "time_window"]]
+submission.TravelTimeSubmission.save_df_travel_time_submission(submission.TravelTimeSubmission, dfResult)
