57bd943a869783509e700bf76f6c4832ddca6c7a
Mihaela Hanea <hanea@cip.ifi.lmu.de>
2017-05-22 19:07:21 +0200

    Update get_traffic.py Added list structure to input Changed file path

--- python/traffic-prediction/src/get_traffic.py
+++ python/traffic-prediction/src/get_traffic.py
@@ -2,6 +2,25 @@
 Created on Wed May 17 14:40:12 2017
 
 @author: Mihaela
+
+Filters the traffic from specific weekdays, between specific hours
+
+Input:
+    Structure of lists:
+        weekdays =[day_of_week, ...]
+                 =[0-6, ...]                                Monday = 0, Tuesday = 1, ..., Sunday = 6
+
+
+        times = [[(hour, minute), (hour,minute)], ...]
+              = [[(0-23,0-59), (0-23,0-59)], ...]           each sublist contains two tuples
+                                                            first tuple = start time
+                                                            second tuple = end time
+
+    example:
+        weekdays = [0,1]
+        times = [[(6,00), (7,50)], [(12,00), (12,20)]]
+
+        get_traffic(weekdays, times) filters the traffic from Mondays and Tuesdays between 6:00-7:50 and 12:00-12:20
 """
 
 import csv as csv
@@ -11,20 +30,31 @@ import os
 from datetime import datetime
 
 def get_traffic(weekdays, times):
-    path_trajectories = '../../../dataset/training/trajectories(table 5)_training.csv'
 
-    df = pd.DataFrame.from_csv(path_trajectories, index_col=[0,1,2])
+
+    if os.name == 'nt':
+        training_files = "../dataset/training/"
+    else:
+        training_files = "../../../dataset/training/"
+
+    trajectories_training_file = "trajectories(table 5)_training.csv"
+
+    trajectories_df = pd.DataFrame.from_csv(training_files+trajectories_training_file_file)
+
+    df = trajectories_df
+
 
     # transform starting_time column from string to timestamp
     df['starting_time'] = pd.to_datetime(df['starting_time'])
 
-    # extract separately date and time from timestamp
-    extracted_time = df['starting_time'].dt.hour
+    # extract separately date and time (hour, minutes) from timestamp
+    extracted_hour = df['starting_time'].dt.hour
+    extracted_minute = df['starting_time'].dt.minute
     extracted_day = df['starting_time'].dt.dayofweek
 
-    # add 'extracted_day' and 'extracted_time' columns to dataset
+    # add 'extracted_day' and 'hour_min_tuples' columns to dataset
     df['extracted_day'] = extracted_day
-    df['extracted_time'] = extracted_time
+    df['hour_min_tuples'] = list(zip(extracted_hour, extracted_minute))
 
     # craete day_mask
     day_mask = df['extracted_day'].isin(weekdays)
@@ -32,23 +62,19 @@ def get_traffic(weekdays, times):
     # create time_mask
     time_mask = [False] * df.shape[0]
     for i in times:
-        time_mask = time_mask | ((df['extracted_time'] + 1 > int(i[0])) & (df['extracted_time'] + 1 <= int(i[1])))
+        time_mask = time_mask | ((df['hour_min_tuples'] > i[0]) & (df['hour_min_tuples'] <= tuple(np.subtract(i[1],(0,1)))))
 
     #create complete_mask
     complete_mask = day_mask & time_mask
 
     #take last added columns out
-    del df['extracted_time']
     del df['extracted_day']
+    del df['hour_min_tuples']
 
     #output
-    return(df.loc[complete_mask])
+    print(df.loc[complete_mask])
 
 
-#test
-days = [0,1]
-hours = [(0,2), (5,8), (20,21)]
-get_traffic(weekdays=days, times=hours)
 
 
         
