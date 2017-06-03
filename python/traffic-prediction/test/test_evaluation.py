0f75fb7d528a2a4ab9b1d754e74d36b067fefcc6
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-06-03 19:08:18 +0200

    first version of MAPE

---
+++ python/traffic-prediction/test/test_evaluation.py
@@ -0,0 +1,17 @@
+import unittest
+import evaluation as evaluation
+import numpy as np
+
+class TestEvaluation(unittest.TestCase):
+
+    def test_get_simple_result(self):
+        y_true = np.array([1, 2, 3, 4])
+        y_pred = np.array([-1, -2, -3, -4])
+        self.assertEqual(evaluation.mape(y_true, y_pred), 2)
+
+    def self_test(self):
+        y_true = np.array([1, 2, 3, 4])
+        self.assertEqual(evaluation.mape(y_true, y_true), 0.0)
+
+if __name__ == '__main__':
+    unittest.main()
