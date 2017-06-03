a410aa58ee4412a48bd9174a4c82a5cfc39ba673
Christian Lemke <lemkech@gmail.com>
2017-06-03 02:03:11 +0200

    SAMBA-26 first version
    data cleaning
    add weather path

--- python/traffic-prediction/src/Paths.py
+++ python/traffic-prediction/src/Paths.py
@@ -5,4 +5,6 @@ training_files = dataset + "/training/"
 testing_files = dataset + "/testing_phase/"
 
 trajectories_training_file = training_files + "trajectories(table 5)_training.csv"
+weather_training_file = training_files + "weather (table 7)_training_update.csv"
 trajectories_testing_file = testing_files + "trajectories(table 5)_test2.csv"
+
