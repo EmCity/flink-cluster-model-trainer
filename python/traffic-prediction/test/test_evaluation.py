7262258b08855abad44e14e43802958e704a383a
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-06-05 19:57:04 +0200

    first draft for the vector implementation

--- python/traffic-prediction/test/test_evaluation.py
+++ python/traffic-prediction/test/test_evaluation.py
@@ -2,6 +2,7 @@ import unittest
 import evaluation as evaluation
 import numpy as np
 
+
 class TestEvaluation(unittest.TestCase):
 
     def test_get_simple_result(self):
@@ -12,6 +13,6 @@ class TestEvaluation(unittest.TestCase):
     def self_test(self):
         y_true = np.array([1, 2, 3, 4])
         self.assertEqual(evaluation.mape(y_true, y_true), 0.0)
-
+        
 if __name__ == '__main__':
     unittest.main()
---
+++ python/traffic-prediction/test/test_generateCurrentSituationVector.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestEvaluation(unittest.TestCase):
+    def test_get_simple_result(self):
+        "todo"
+if __name__ == '__main__':
+    unittest.main()
