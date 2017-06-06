cbb1575cbbdc2b63053bc62bdf04fdaa56201399
Mihaela Hanea <hanea@cip.ifi.lmu.de>
2017-06-06 15:22:05 +0200

    Name changed, create_20min_partition deleted, modified generation of X and Y

--- python/traffic-prediction/src/VectorGeneration_xAsTimeInformation.py
+++ python/traffic-prediction/src/VectorGeneration_xAsTimeInformation.py
@@ -11,78 +11,31 @@ The generate_timeInformationVector() function generates two numpy arrays:
 """
 import numpy as np
 import pandas as pd
-import itertools
-import get_traffic as traffic
 import Paths as path
 
 
 
-def create_20min_partitions(hours):
-    """
-    splits a timestamp in 20 minutes intervals
+def generate_timeInformationVector(df):
+    df['starting_time'] = df['starting_time'].astype('datetime64[ns]')
 
-    :param hours:
-                tuple containing start and end time
-                example: (3,5)  start time = 3am; end time = 5am
+    #Get a 20 min sliding window for the dataframe
+    df_group = df.groupby([pd.Grouper(key='starting_time', freq='20min')])
 
-    :return: list with 20 minutes time intervals
-                example: for hours=(2,3), output = [[(2, 0), (2, 20)], [(2, 20), (2, 40)], [(2, 40), (3, 0)]]
-    """
+    #Generate X
+    first_row = list(df_group['starting_time'].first())
+    day_hour_tuple = [[first_row[i].dayofweek, first_row[i].hour] for i in range(0, len(first_row))]
+    X = day_hour_tuple[::6]
+    X = [j for i in X for j in i]
+    X = np.array(X)
 
-
-    h = list(range(hours[0],hours[1]))
-    m = [0, 20, 40]
-
-    times = list(itertools.product(h, m))
-    times_list = [list(i) for i in zip(times, times[1:])]
-    times_list.append([(hours[1]-1,40), (hours[1],0)])
-
-    return(times_list)
+    #Generate Y
+    average_time = list(df_group['travel_time'].mean())
+    Y = [average_time[n:n+6] for n in range(0, len(average_time), 6)]
+    Y = np.array(Y)
 
 
-print(create_20min_partitions((2,4)))
-
-def generate_timeInformationVector(hours, days):
-    """
-    generates
-    x = time information
-    y = avarage travel time
-
-    !!! uses trajectories_training_file as dataset !!!
-
-    :param hours:
-                tuple containing start and end time
-                example: (3,5)  start time = 3am; end time = 5am
-    :param days:
-                list of weekdays
-                example: [0,1] takes ony Monday and Tuesdays into consideration
-
-    :return:
-    """
-
-
-    trajectories_df = pd.read_csv(path.trajectories_training_file)
-    df = trajectories_df
-
-    df['starting_time'] = pd.to_datetime(df['starting_time'])
-
-
-    x_initial = np.array([]).astype(int)
-    y_initial = np.array([])
-    for d in days:
-        for t in create_20min_partitions(hours):
-            partition_20min = traffic.get_traffic([d], [t])
-            x_initial = np.append(x_initial, (d, t[0][0]))
-            y_initial = np.append(y_initial, partition_20min['travel_time'].mean())
-
-
-    x = np.array([]).astype(int)
-    for v, w in zip(x_initial[::12],x_initial[1::12]):
-        x = np.append(x, (v, w))
-
-    y = [y_initial[n:n+6] for n in range(0, len(y_initial), 6)]
-
-    return(x, y)
+    return(X, Y)
 
 #test
-#print(generate_timeInformationVector(hours = (2,4), days = [1,2]))
+print(generate_timeInformationVector(pd.read_csv(path.trajectories_testing_file)))
+
