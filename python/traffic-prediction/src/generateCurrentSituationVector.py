3089d529db1cc19e0c7ee019f4eb031d610b4329
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-06-06 12:31:28 +0200

    added comments/refactored

--- python/traffic-prediction/src/generateCurrentSituationVector.py
+++ python/traffic-prediction/src/generateCurrentSituationVector.py
@@ -7,28 +7,45 @@ def generate_vector(df_orig):
     #Generate X
     df = prepare_df_travelseq(df_orig)
     df['starting_time'] = df['starting_time'].astype('datetime64[ns]')
+
+    #Get a 20 min sliding window for the dataframe
     df_group = df.groupby([pd.Grouper(key='starting_time', freq='20min')])
     df['link_travel_time'] = pd.to_numeric(df['link_travel_time'])
 
     mylist_X = []
+    #Iterate over a sliding window dataframe
     for name, group in df_group:
+        #Get the averages travel time per link
         df_temp = group.groupby(['link'])['link_travel_time'].mean().reset_index(name="avg_travel_time")
         np_arr = df_temp.as_matrix(columns=['avg_travel_time'])
+
+        #Add the averages per link to the List
         mylist_X.append(np_arr)
-    X = np.array(mylist_X)
+
+    #Concatenate the X vector (np array) from the list of numpy arrays
+    X = np.concatenate(mylist_X)
+    #print(X)
 
     #Generate Y
     df_orig['starting_time'] = df_orig['starting_time'].astype('datetime64[ns]')
+
+    #Get a 20 min sliding window for the dataframe
     df_orig_group = df_orig.groupby([pd.Grouper(key='starting_time', freq='20min')])
 
     mylist_Y = []
+    #Iterate over a sliding window dataframe
     for name, group in df_orig_group:
+        #Get the averages travel time route
         df_temp = group.groupby(['intersection_id', 'tollgate_id'])['travel_time'].mean().reset_index(name="avg_travel_time")
         np_arr = df_temp.as_matrix(columns=['avg_travel_time'])
         mylist_Y.append(np_arr)
-    Y = np.array(mylist_Y)
-    return X, Y
 
+    #Delete the first 6 elements because we are not interested in the first 6 time windows (first 2 hours)
+    del mylist_Y[0:5]
+
+    #Concatenate the Y vector (np array) from the list of numpy arrays
+    Y = np.concatenate(mylist_Y)
+    return X, Y
 
 def prepare_df_travelseq(df):
 
