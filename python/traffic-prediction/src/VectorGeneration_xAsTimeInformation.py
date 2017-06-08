3433c9983083c5881623f6aefe80395b0356becc
Mihaela Hanea <hanea@cip.ifi.lmu.de>
2017-06-08 07:57:39 +0200

    Fixed Y vector

--- python/traffic-prediction/src/VectorGeneration_xAsTimeInformation.py
+++ python/traffic-prediction/src/VectorGeneration_xAsTimeInformation.py
@@ -6,13 +6,12 @@ The generate_timeInformationVector() function generates two numpy arrays:
 
         2. y array
                 contains the average travel time for every 20 minutes
-                each row is a subarray containing
-                    6 x avg_traveltime = [avg_traveltime(first_20min), avg_traveltime(second_20min), ... ]
+                [route1_avg_travel_time(time_window_1), route2_avg_travel_time(time_window_1), ... , route6_avg_travel_time(time_window_6)]
+                array length per 2 hours = 6 time windows a 20 minutes * 6 routes = 36
 """
 import numpy as np
 import pandas as pd
 import Paths as path
-
 
 
 def generate_timeInformationVector(df):
@@ -26,14 +25,25 @@ def generate_timeInformationVector(df):
     day_hour_tuple = [[first_row[i].dayofweek, first_row[i].hour] for i in range(0, len(first_row))]
     X = day_hour_tuple[::6]
     X = [j for i in X for j in i]
+
+    #delete last 2 h of X -> no prediction can be made on this data
+    #del X[-2:]
+
+    #build X array
     X = np.array(X)
 
+    Y = []
     #Generate Y
-    average_time = list(df_group['travel_time'].mean())
-    Y = [average_time[n:n+6] for n in range(0, len(average_time), 6)]
-    Y = np.array(Y)
+    for name, group in df_group:
+        df_temp = group.groupby(['intersection_id', 'tollgate_id'])['travel_time'].mean().reset_index(name="avg_travel_time")
+        np_arr = df_temp['avg_travel_time'].tolist()
+        Y.append(np_arr)
 
+    #delete first 2h of Y -> no prediction can be made on this data
+    #del Y[0:5]
 
+    #build  Y
+    Y = np.concatenate(Y)
     return(X, Y)
 
 #test
