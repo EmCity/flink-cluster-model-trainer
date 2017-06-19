aa5000b1422c12b6d57b4050e6e2b0cc2792edb2
amaurino <antoine.maurino@gmail.com>
2017-06-19 08:52:17 +0200

    merge

--- python/traffic-prediction/src/models/SVM_currentSituation.py
+++ python/traffic-prediction/src/models/SVM_currentSituation.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 
 df = pd.DataFrame.from_csv(path.trajectories_training_file)
-print(df)
-#X,Y = vec.generate_vector(df)
-#print(X,Y)
+#print(df)
+X,Y = vec.generate_vector(df)
+print(X,Y)
 #X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,test_size=0.2)
 #training, validation, testing = split.split_dataset(df, 0.8, 0.1)
 # X_train,Y_train = vec.generate_vector(training)
