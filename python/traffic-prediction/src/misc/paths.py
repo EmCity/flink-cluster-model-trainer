09e2b368293fa2ace64c3c5a46e21bcf5c4eb128
amaurino <antoine.maurino@gmail.com>
2017-06-19 19:17:42 +0200

    merge

--- python/traffic-prediction/src/misc/paths.py
+++ python/traffic-prediction/src/misc/paths.py
@@ -1,10 +1,21 @@
 #Path file for the used data paths
 __dataset = '../../../../dataset'
+__new_dataset = '../../../../new_dataset'
 
+#last 3months
 __training_files = __dataset + "/training/"
 __testing_files = __dataset + "/testing_phase1/"
+
+#new dataset with the week added
+__new_training_files = __new_dataset + "/training/"
+__new_testing_files = __new_dataset + "/testing_phase/"
+
+
 
 trajectories_training_file = __training_files + "trajectories(table 5)_training.csv"
 weather_training_file = __training_files + "weather (table 7)_training.csv"
 trajectories_testing_file = __testing_files + "trajectories(table 5)_test1.csv"
-trajectories_training_file2 ="../../../../dataset/training2/trajectories(table_5)_training2.csv"
+trajectories_training_file2 = __dataset + "/training2/trajectories(table_5)_training2.csv"
+trajectories_testing_file2 = __dataset + "/training2/trajectories(table_5)_training2.csv"
+
+
--- python/traffic-prediction/src/vector_gen/generateCurrentSituationWithTime.py
+++ python/traffic-prediction/src/vector_gen/generateCurrentSituationWithTime.py
@@ -8,15 +8,15 @@ from misc import paths as path
 def generate_vector(df):
     x = vec.generate_x_df(df)
     x_2 = vec2.generate_timeInformationVector(df)
-    weekdays = x_2[0::3]
-    hours = x_2[1::3]
-    minutes = x_2[2::3]
-    x['weekday'] = weekdays
-    x['hours'] = hours
-    x['minutes'] = minutes
+    print(x)
+    print("hhhhhhhhhhhhh")
+    print(x_2)
+    print(type(x_2))
+    #x_3 =  pd.concat([x, x_2], axis=1)
+    #print(x_3)
 
     return x
 
 df = pd.read_csv(path.trajectories_training_file2)
 x = generate_vector(df)
-print(x.to_string)
+#print(x.to_string)
