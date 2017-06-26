e00090f9d9a1d9bac576ca646abe4890c010fd59
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-06-26 18:11:21 +0200

    updated the java file

--- flink/flink-python-job/src/main/java/org/lmu/RunCMD2.java
+++ flink/flink-python-job/src/main/java/org/lmu/RunCMD2.java
@@ -1,22 +1,14 @@
 package org.lmu;
-
-import org.apache.avro.data.Json;
 import org.apache.flink.api.java.ExecutionEnvironment;
-
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.util.Collector;
 import org.lmu.JSON.JSONArray;
 import org.lmu.JSON.JSONObject;
 import org.lmu.JSON.parser.JSONParser;
-import org.mortbay.util.ajax.JSON;
-
-
 import java.io.FileReader;
-import java.util.Locale;
 import java.util.*;
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -28,10 +20,9 @@ public class RunCMD2 {
 	public static void main(String[] args) throws Exception {
 		// set up the batch execution environment
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-		String jsonPath = args[0];
 		JSONObject json;
-		if(jsonPath!=null){
-			 json = (JSONObject) new JSONParser().parse(new FileReader(jsonPath));
+		if(args[0]!=null){
+			 json = (JSONObject) new JSONParser().parse(new FileReader(args[0]));
 		}
 		else{
 			throw new Exception("NO JSON PATH AS INPUT");
@@ -39,24 +30,13 @@ public class RunCMD2 {
 
 		JSONObject algorithms = (JSONObject) json.get("algorithms");
 		JSONObject data = (JSONObject) json.get("data");
-
 		JSONObject svm = (JSONObject) algorithms.get("SVM");
 		JSONObject lr = (JSONObject) algorithms.get("LR");
 		JSONObject nn = (JSONObject) algorithms.get("NN");
-
-		System.out.println(lr);
-
 		JSONArray svmArray = svm!= null ? createSVMJobs(svm) : new JSONArray();
 		JSONArray lrArray = lr != null ? createLRJobs(svm) : new JSONArray();
 		JSONArray nnArray = nn != null ? createNNJobs(svm) : new JSONArray();
 
-		if(args.length > 0){
-			System.out.println("----args:" + args[0]);
-		}else{
-			System.out.println("----args: none");
-		}
-
-		// DataSet<String> text = env.readTextFile("path/to/file");
 		// get input data
 		ArrayList<String> tasks = new ArrayList<>();
 		fillList(data, svmArray, tasks);
@@ -64,15 +44,15 @@ public class RunCMD2 {
 		fillList(data, nnArray, tasks);
 
 		DataSet<String> elementsDataSet = env.fromCollection(tasks);
-		DataSet<String> res = elementsDataSet.flatMap(new OnWorkers());
-		// text.writeAsCsv(outputPath, "\n", " ");
 
+		//distribute on workers
+		DataSet<String> res = elementsDataSet.flatMap(new OnWorkers());
+
+		//saves the result as text
+		res.writeAsText("result.txt", FileSystem.WriteMode.OVERWRITE);
 
 		// execute program
-		//env.execute("Flink Batch RunCMD Job");
-		
-		// execute and print result
-        res.print();
+		env.execute("Flink Batch RunCMD Job");
 	}
 
 	private static void fillList(JSONObject data, JSONArray array, ArrayList<String> tasks) {
@@ -80,7 +60,6 @@ public class RunCMD2 {
 			obj = new JSONObject((JSONObject)obj);
 			((JSONObject) obj).put("data", data);
 			tasks.add(obj.toString());
-			System.out.println(obj);
 		}
 	}
 
@@ -99,25 +78,16 @@ public class RunCMD2 {
 				result.add(obj);
 			}
 		}
-
 		return result;
 	}
 
 	private static JSONArray createLRJobs(JSONObject lr) {
 		Boolean normalize = Boolean.parseBoolean((String)lr.get("normalize"));
 		JSONArray result = new JSONArray();
-		//for(int i = 0; i < cParams.size(); i++){
-		//	for(int j = 0; j< eParams.size(); j++){
-		//		Long c = (Long)cParams.get(i);
-		//		Double e = (Double)eParams.get(j);
-				JSONObject obj = new JSONObject();
+		JSONObject obj = new JSONObject();
 		obj.put("algorithm", "LR");
 		obj.put("normalize", normalize);
-		//obj.put("E", e);
 		result.add(obj);
-		System.out.println(result);
-		//	}
-		//}
 		return result;
 	}
 
@@ -139,21 +109,19 @@ public class RunCMD2 {
 			Runtime rt = Runtime.getRuntime();
 			String[] commands = {"system.exe","-get t"};
 			String cmd1 = "pwd";
+			String cmd = "python3";
+			String pythonPath = "../BigDataScience/sose17-small-data/python/traffic-prediction/src/flink/trainModel.py";
 
 			// path depends on the folder where the command of flink run was called
-			String cmd2 = "python3 ../BigDataScience/sose17-small-data/python/traffic-prediction/src/flink/mysklearntest.py";
+			String cmd2 = cmd + " " + pythonPath + " " + value;
 
-			
 			Process proc = rt.exec(cmd2);
-
 			BufferedReader stdInput = new BufferedReader(new
 					InputStreamReader(proc.getInputStream()));
 
 			BufferedReader stdError = new BufferedReader(new
 					InputStreamReader(proc.getErrorStream()));
 
-
-			
 			String res = "";
 			
 			// read the output from the command
@@ -172,28 +140,9 @@ public class RunCMD2 {
 				res += "\n"+s;
 			}
 
-			
-
 			out.collect(res);
 			} catch(Exception e){
-				
 			}
-			
-			/*
-            // normalize and split the line
-            String[] tokens = value.toLowerCase(Locale.US).split("\\W+");
-
-            // emit the pairs
-            for (String token : tokens) {
-                if (token.length() > 0) {
-                    out.collect(new Tuple2<String, Integer>(token, 1));
-                }
-            }
-            */
-            
         }
     }
-	
-	
-	
 }
