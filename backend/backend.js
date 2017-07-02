012102a2580cf98e13f2a6cf0a77829409ad3a18
root <root@vm-141-40-254-102.cloud.mwn.de>
2017-07-02 12:40:49 +0200

    getresults

--- backend/backend.js
+++ backend/backend.js
@@ -13,16 +13,7 @@ app.get('/', function(req, res) {
   res.sendFile('/root/code/sose17-small-data/gui/src/index.html') 
 });
 
-// save AlgoParaImputs?
-//app.get('/', (req, res) => {
- // console.log(typeof(req))
- // console.log(req.body)
-  //data = req.body;
- // res.send("node is running. indexhtml will get here");
-//});
-
-
-// save AlgoParaImputs?
+// save AlgoParaImputs
 app.post('/api/', (req, res) => {
   console.log(typeof(req))
   console.log(req.body)
@@ -37,6 +28,7 @@ app.post('/api/', (req, res) => {
     res.send(req.body);
 });
 
+// not in use
 app.post('/save_result/', (req, res) => {
   console.log(typeof(req))
   console.log(req.body)
@@ -50,7 +42,29 @@ app.post('/save_result/', (req, res) => {
     //res.send(req.body);
     res.send('ok-....'+ req.body);
 });
+// get results
+app.get('/get_results/', (req, res) => {
+  var x = "";  
+  MongoClient.connect(url, function(err, db) {
+      var col = db.collection('results');
+      x += "error: " + err + "<br>";
+      
+      var cursor =  col.find({});
+      cursor.toArray(function(err, result){
+          x = result;
+          res.send(x);   
+          console.log(result);
 
+          db.close();
+      });
+
+    });
+});
+
+
+app.post('/test/', function (req, res) {
+    console.log('works');
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
