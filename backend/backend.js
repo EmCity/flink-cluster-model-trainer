b0911377b6eddbaddb926db4d112f7845656292d
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-07-03 11:43:49 +0200

    implemented "calling flink from node.js"

--- backend/backend.js
+++ backend/backend.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var child = require('child_process')
 var app = express();
 app.use(bodyParser.json()); // add a middleware (so that express can parse request.body's json)
 const hostname = 'sambahost.dyndns.lrz.de';
@@ -24,9 +25,34 @@ app.post('/api/', (req, res) => {
     db.collection("jobs").insertOne(data);
     db.collection("jobs").find().sort({timestart:-1},function(err,cursor){});
     db.close();
+    console.log(data.job_name)
+    callFlink(data.job_name)
   });
     res.send(req.body);
+
+
 });
+
+// save AlgoParaImputs
+app.get('/start_job/:job_name', (req, res) => {
+    console.log(req.params.job_name);
+    callFlink(req.params.job_name);
+
+});
+
+
+
+function callFlink (jobname) {
+    child.exec('flink-1.3.0/bin/flink run -c org.lmu.JobNameBatchDB ' +
+        '~/code/sose17-small-data/flink/flink-python-job/target/flink-python-job-0.1.jar' + jobname, (error, stdout, stderr) =>{
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+}
 
 // not in use
 app.post('/save_result/', (req, res) => {
