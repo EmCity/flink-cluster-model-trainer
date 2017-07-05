1b66269fe06cd298cc1617f992c0186fbe0aec0b
Christian Lemke <lemkech@gmail.com>
2017-07-05 12:26:06 +0200

    csvheader1

--- python/traffic-prediction/src/flink/trainModel.py
+++ python/traffic-prediction/src/flink/trainModel.py
@@ -48,10 +48,10 @@ def trainModel(jsonDict):
     #file.write(test_x)
     #file.close();
 
-    df_x_train = pd.read_csv(train_x_io, index_col=0, sep=',', lineterminator='\r\n', header=0)
-    df_x_test = pd.read_csv(test_x_io, index_col=0, sep=',', lineterminator='\r\n', header=0)
-    df_y_train = pd.read_csv(train_y_io, index_col=0, sep=',',lineterminator='\r\n', header=0)
-    df_y_test = pd.read_csv(test_y_io, index_col=0, sep=',',lineterminator='\r\n', header=0)
+    df_x_train = pd.read_csv(train_x_io, index_col=0, sep=',', lineterminator='\n', header=0)
+    df_x_test = pd.read_csv(test_x_io, index_col=0, sep=',', lineterminator='\n', header=0)
+    df_y_train = pd.read_csv(train_y_io, index_col=0, sep=',',lineterminator='\n', header=0)
+    df_y_test = pd.read_csv(test_y_io, index_col=0, sep=',',lineterminator='\n', header=0)
 
     result_mape = ""
 
