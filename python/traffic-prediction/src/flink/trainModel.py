2ff31c11ab45c2b7d208bb404e6010e5cdcd6c61
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-07-13 17:50:12 +0200

    changes to NN

--- python/traffic-prediction/src/flink/trainModel.py
+++ python/traffic-prediction/src/flink/trainModel.py
@@ -142,7 +142,7 @@ def train_nn(df_x_train, df_x_test, df_y_train, df_y_test, params):
                 avg_cost += c
         # Test model
         prediction = pred.eval(feed_dict={x: df_x_test}, session=sess)
-        mape = eval.mape(prediction, df_y_test)
+        mape = np.mean(np.abs((df_y_test - prediction) / df_y_test))
         return np.mean(mape)
 
 
