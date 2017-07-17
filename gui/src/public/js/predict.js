0dedb172b41786d08b6ccbef0d5105b4f6bf7419
Christian Lemke <lemkech@gmail.com>
2017-07-17 23:28:50 +0200

    bug02

--- gui/src/public/js/predict.js
+++ gui/src/public/js/predict.js
@@ -1,13 +1,13 @@
 function predict() {
-    data = new Object();
-    data.data = new Object();
-    data.data.train_x = new Object();
-    data.data.train_y = new Object();
-    data.data.test_x = new Object();
-    data.data.test_y = new Object();
-    data.data.valid_x = new Object();
-    data.data.valid_y = new Object();
-    let a = handleFileSelect("trainingXFile");
+  data = new Object();
+  data.data = new Object();
+  data.data.train_x = new Object();
+  data.data.train_y = new Object();
+  data.data.test_x = new Object();
+  data.data.test_y = new Object();
+  data.data.valid_x = new Object();
+  data.data.valid_y = new Object();
+  let a = handleFileSelect("trainingXFile");
     let b = handleFileSelect("trainingYFile");
     let c = handleFileSelect("testingXFile");
     let d = handleFileSelect("testingYFile");
@@ -25,43 +25,43 @@ function predict() {
             data.algorithms[algoID][id] = new Object();
             if ($(this).is(':checkbox'))
                 data.algorithms[algoID][id] = ($(this).prop('checked') == true);
-
             else {
                 if (id == "gamma" && !($(this).val().length))
                     data.algorithms[algoID][id] = "auto";
-                else if ($(this).val().split(',').length > 1) {
+                else if($(this).val().split(',').length > 1)
+                {
                     var array = $(this).val().split(',')
                     console.log("Was parsable as String");
                     data.algorithms[algoID][id] = array;
-                } else {
+                }
+                else
+                {
                     //data.algorithm[algoID][id] = "[".concat($(this).val(), "]");
                     var array = [$(this).val()];
                     data.algorithms[algoID][id] = array;
                 }
-
             }
         });
     });
-    if (data.algorithms['SVM']) {
-        data.algorithms['SVM']['tolerance'] = data.algorithms['SVM']['tolerance'][0];
-        data.algorithms['SVM']['cache_size'] = data.algorithms['SVM']['cache_size'][0];
-        data.algorithms['SVM']['max_iter'] = data.algorithms['SVM']['max_iter'][0];
+     if(data.algorithms['SVM'])
+     {
+          data.algorithms['SVM']['tolerance'] = data.algorithms['SVM']['tolerance'][0];
+          data.algorithms['SVM']['cache_size'] = data.algorithms['SVM']['cache_size'][0];
+          data.algorithms['SVM']['max_iter'] = data.algorithms['SVM']['max_iter'][0];
+     }
 
-    }
+     var snackbarContainer = document.querySelector('#toast');
+     var toastdata = {
+             message: 'Thanks for submitting your job! You’ll be able to see it in the results tab.'
+         };
+     snackbarContainer.MaterialSnackbar.showSnackbar(toastdata );
 
-    var snackbarContainer = document.querySelector('#toast');
-    var data = {
-            message: 'Job \"' + data.job_name + '\" is submitted. Computing...'
-        };
-    snackbarContainer.MaterialSnackbar.showSnackbar(data);
-
-    Promise.all([a, b, c, d]).then(function() {
+     Promise.all([a, b, c, d]).then(function() {
         fetch("http://sambahost.dyndns.lrz.de:8500/api", {
             method: 'POST',
             body: JSON.stringify(data), // stringify JSON
             headers: new Headers({
-                "Content-Type": "application/json",
-                'Access-Control-Allow-Origin': '*'
+                "Content-Type": "application/json", 'Access-Control-Allow-Origin':'*'
             }) // add headers
         }).then(function(response) {
             // The response is a Response instance.
@@ -70,19 +70,16 @@ function predict() {
         }).then(function(data) {
             // `data` is the parsed version of the JSON returned from the above endpoint.
             console.log(data); // { "userId": 1, "id": 1, "title": "...", "body": "..." }
+
+            var toastdata2 = {
+                message: 'Job '  + ' error.'
+            };
+            snackbarContainer.MaterialSnackbar.showSnackbar(toastdata2);
         }).catch(function(error) {
             console.log('Request failed', error);
-
-            var data = {
-                message: 'Job ' + 'data.job_name' + ' error.'
-            };
-            snackbarContainer.MaterialSnackbar.showSnackbar(data);
         })
     })
-
-
 }
-
-function browse_fct() {
-    window.open('get_results', '_blank');
+function browse_fct(){
+        window.open('get_results','_blank');
 }
